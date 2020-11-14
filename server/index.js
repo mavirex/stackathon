65ed89dd6a9f1abe027148ce11988a97dc687a35
Mark Guinn <mxavier927@gmail.com>
2020-11-14 11:29:36 -0500

    installed path

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ app.use(express.json())
 
 app.use(express.static(path.join(__dirname, '/public')))
 app.use('/api', api);
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, './public/index.html'))
 })
 
