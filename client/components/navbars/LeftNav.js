3dd1b62204c838a0421235d0c04df2b8b1d7510c
Mark Guinn <mxavier927@gmail.com>
2020-11-14 11:57:40 -0500

    can recharge quantum blasts

--- client/components/navbars/LeftNav.js
+++ client/components/navbars/LeftNav.js
@@ -1,9 +1,27 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { getBounty } from '../../redux/bounties/bounty'
+import { updateUser } from '../../redux/users/user'
 
 class LeftNav extends Component {
+  rechargeBlasts () {
+    const { user, updateUser } = this.props
+    if (user.credits < 100) {
+      alert("It costs 100 credits per Quantum Blast")
+    } else if (user.quantumBlasts >= 10) {
+      alert("You can only have a maximum of 10 Quantum Blasts")
+    } else {
+      const credits = user.credits - 100
+      const quantumBlasts = user.quantumBlasts + 1
+      updateUser(user.id, {
+        credits,
+        quantumBlasts
+      })
+    }
+    console.log('Hello World')
+  }
   render () {
+    const { rechargeBlasts } = this
     const { user, getBounty } = this.props
     return (
       <div className='sidenav leftnav'>
@@ -16,7 +34,10 @@ class LeftNav extends Component {
           </ul>
         </div>
         <div>Credits: {user.credits}</div>
-        <div>Quantum Blasts:{user.quantumBlasts}</div>
+        <div>
+          <div>Quantum Blasts:{user.quantumBlasts}</div>
+          <button onClick={rechargeBlasts}>Recharge Blasts (100 Credits)</button>
+        </div>
       </div>
     )
   }
@@ -29,7 +50,10 @@ const mapStateToProps = (state) => ({
 const mapDispatchToProps = (dispatch) => ({
     getBounty: (difficulty) => {
         dispatch(getBounty(difficulty))
-    }
+    },
+    updateUser: (userId, newUserInfo) => {
+      dispatch(updateUser(userId, newUserInfo))
+    },
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(LeftNav)
